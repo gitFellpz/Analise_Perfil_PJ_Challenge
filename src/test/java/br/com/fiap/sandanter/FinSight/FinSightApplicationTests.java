a3ec4f858dbd4fd9c839d4709ce1fd60af6e85b9
gitFellpz <felipe.adrilima@gmail.com>
2025-05-25 20:05:35 -0300

    Criando projeto

---
+++ src/test/java/br/com/fiap/sandanter/FinSight/FinSightApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.sandanter.FinSight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinSightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
